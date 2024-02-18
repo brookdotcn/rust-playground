54810fbefbeb2261ff4db3443d00afc68c840e0c
Brook Nash <brookdotcn@outlook.com>
2024-02-18 22:34:45 +0000

    feat: module selection from cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::collections::HashMap;
+
+fn example_module() -> () {
     println!("Hello, world!");
+}
+
+fn main() -> () {
+    let key_arg = match std::env::args().nth(1) {
+        Some(k) => k,
+        None => panic!("no module key passed"),
+    };
+
+    let mut modules: HashMap<String, fn()> = HashMap::new();
+    modules.insert("example".to_string(), example_module);
+
+    match modules.get(&key_arg) {
+        Some(f) => f(),
+        None => panic!("invalid module key"),
+    };
 }
