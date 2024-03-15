85a573b761ff00fb347ad91f73cdd7c90437faae
Brook Nash <brookdotcn@outlook.com>
2024-03-15 16:08:32 +0000

    feat: hangman game

---
+++ src/game/hangman.rs
@@ -0,0 +1,119 @@
+use rand::Rng;
+use std::io::{stdin, stdout, Write};
+
+fn clear_console() -> () {
+    print!("\x1B[2J\x1B[1;1H");
+}
+
+fn read_guess() -> String {
+    let mut text = String::new();
+    print!(" | [GUESS] ");
+
+    stdout().flush().unwrap();
+    stdin().read_line(&mut text).unwrap();
+
+    text
+}
+
+/// iterate over each `char` and print it if guessed, otherwise an underscore.
+/// * **word** `&str` the chosen word
+/// * **correct_chars** `&Vec<char>` all correct guesses
+fn display_word(word: &str, correct_chars: &Vec<char>) -> () {
+    for i in 0..word.len() {
+        let char = word.chars().nth(i).unwrap();
+        if correct_chars.contains(&char) {
+            print!("{char}");
+            continue;
+        }
+
+        print!(" _ ");
+    }
+}
+
+/// count the matches of a guess within a word.
+/// * **word** `&str` the chosen word
+/// * **guess** `char` match to find in **word**
+fn find_occurences(word: &str, guess: char) -> usize {
+    word.matches(guess).collect::<Vec<&str>>().len()
+}
+
+/// validate guess is present in a word.
+/// * **word** `&str` the chosen word
+/// * **guess** `char` match to find in **word**
+fn guess_is_valid(word: &str, guess: char) -> bool {
+    word.find(guess).is_some()
+}
+
+/// randomly select a word from a list.
+/// * **words** `[&str]` list of words to use in the game
+fn choose_word<'l>(words: &'l [&str; 5]) -> &'l str {
+    let index = rand::thread_rng().gen_range(0..words.len());
+    words[index]
+}
+
+pub fn hangman_run() -> () {
+    let words: [&str; 5] = ["rust", "trait", "arc", "impl", "match"];
+    let word = choose_word(&words);
+
+    let mut correct_guesses = Vec::<char>::new();
+    let mut incorrect_guesses = Vec::<char>::new();
+    let mut error = String::new();
+
+    clear_console();
+
+    loop {
+        clear_console();
+
+        /* print error after console being cleared */
+        if !error.is_empty() {
+            println!("\x1b[93m{error}\x1b[0m\n");
+        }
+
+        /* handle game result */
+        if correct_guesses.len() == word.len() {
+            println!("[SUCCESS] the word was {word}");
+            break;
+        } else if incorrect_guesses.len() >= 5 {
+            println!("[FAILURE] the word was {word}");
+            break;
+        }
+
+        /* show lives and previous incorrect guesses if an incorrect guess has been made */
+        if incorrect_guesses.len() > 0 {
+            let previous_incorrect = incorrect_guesses
+                .iter()
+                .map(|char| char.to_string())
+                .collect::<Vec<String>>()
+                .join(", ");
+
+            print!("[LIVES] {}", 5 - incorrect_guesses.len());
+            print!(" - ");
+            print!("[INCORRECT] {previous_incorrect}\n\n");
+        }
+
+        display_word(&word, &correct_guesses);
+
+        let guess = read_guess();
+        if guess.len() > 2 || guess.len() == 1 {
+            error = String::from("guess should be a single character");
+            continue;
+        }
+
+        let guess_char = guess.chars().nth(0).unwrap();
+        if !guess_char.is_alphabetic() {
+            error = String::from("guess should be alphabetic");
+            continue;
+        }
+
+        if guess_is_valid(word, guess_char) {
+            for _ in 0..find_occurences(word, guess_char) {
+                correct_guesses.push(guess_char);
+            }
+        } else {
+            incorrect_guesses.push(guess_char);
+        }
+
+        /* reset error after a successful run, incorrect or correct */
+        error.clear();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() -> () {}
+mod game;
+
+use std::collections::HashMap;
+
+fn main() -> () {
+    let module_input = std::env::args().nth(1).expect("no module passed");
+
+    let mut modules = HashMap::<String, fn()>::new();
+    modules.insert("hangman".to_string(), game::hangman::hangman_run);
+
+    match modules.get(&module_input) {
+        Some(f) => f(),
+        None => panic!("unknown module key"),
+    }
+}
